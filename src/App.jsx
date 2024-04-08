2dad05f8ceabf41f9a4ee6c6930f5bb3e126e4e4
Fernando <jesusfgm45@gmail.com>
2024-04-07 23:33:50 -0600

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { data } from './data/data'
+
+function App() {
+  return (
+    <>
+      <div className='divContainer'>
+        <h1>Keywords for the new Google Calendar</h1>
+        {
+          data.map(item => (
+            <div key={item.name} className='dataContainer'>
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <img src={`https://ssl.gstatic.com/tmly/f8944938hffheth4ew890ht4i8/flairs/xxhdpi/img_${item.name}.jpg`} alt={item.name}/>
+            </div>
+          ))
+        }
+      </div>
+      <footer>
+        <a className="read-the-docs">
+          Click here to learn more
+        </a>
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/data/data.js
@@ -0,0 +1,387 @@
+export const data = [
+    {
+      name: "americanfootball",
+      description: "Fútbol americano, fútbol americano, Super Bowl, Superbowl"
+    },
+    {
+      name: "archery",
+      description: "Tiro con arco"
+    },
+    {
+      name: "art",
+      description: "Taller de arte, talleres de arte, taller de dibujo, pintura, taller de dibujo"
+    },
+    {
+      name: "artisticgymnastics",
+      description: "Gimnasia artística"
+    },
+    {
+      name: "athleticsjumping",
+      description: "Salto"
+    },
+    {
+      name: "athleticsthrowing",
+      description: "Disco, lanzamiento de martillo, jabalina, lanzamiento de peso"
+    },
+    {
+      name: "babyshower",
+      description: "Baby shower, babyshower, maternidad"
+    },
+    {
+      name: "backtoschool",
+      description: "Espalda 2 escuela, regreso a la escuela, regreso a la escuela, regreso a la escuela, primer día de clases, primer día de clases"
+    },
+    {
+      name: "badminton",
+      description: "Bádminton"
+    },
+    {
+      name: "baseball",
+      description: "Béisbol"
+    },
+    {
+      name: "basketball",
+      description: "Baloncesto"
+    },
+    {
+      name: "bbq",
+      description: "Barbacoa, barbacoa, barbacoa"
+    },
+    {
+      name: "beer",
+      description: "Cerveza, cervezas, October Fest, Octoberfest, Oktoberfest"
+    },
+    {
+      name: "billiard",
+      description: "Billar"
+    },
+    {
+      name: "bookclub",
+      description: "Club de lectura, lectura"
+    },
+    {
+      name: "bowling",
+      description: "Bolos"
+    },
+    {
+      name: "boxing",
+      description: "Boxeo"
+    },
+    {
+      name: "breakfast",
+      description: "Desayuno, desayunos, brunch, brunch"
+    },
+    {
+      name: "camping",
+      description: "Camping"
+    },
+    {
+      name: "carmaintenance",
+      description: "Mantenimiento de automóviles, mecánico de automóviles, reparación de automóviles, mantenimiento de automóviles, mecánico de automóviles, reparación de automóviles, cambio de llantas, reemplazo de llantas"
+    },
+    {
+      name: "chinesenewyear",
+      description: "Año Nuevo Lunar Chino, año nuevo chino, Año Nuevo Chino, año nuevo chino, año nuevo chino, año nuevo vietnamita"
+    },
+    {
+      name: "cinema",
+      description: "Cine, película, películas"
+    },
+    {
+      name: "clean",
+      description: "Limpiar la casa, limpiar el apartamento, limpiar la casa, ordenar, aspirar, aspirar"
+    },
+    {
+      name: "climbing",
+      description: "Búlder, escalada"
+    },
+    {
+      name: "code",
+      description: "Codecademy, tiempo de codificación, informática, hackathon, Hora del Código, aprender a codificar, Programación en Java, Programación en Python, Rails Girls, Railsgirls, Desarrollo web, Programación web"
+    },
+    {
+      name: "coffee",
+      description: "Café, cafés"
+    },
+    {
+      name: "concert",
+      description: "Concierto, conciertos, conciertos, conciertos"
+    },
+    {
+      name: "cooking",
+      description: "Cocinar la cena, cocinar el almuerzo, cocinar la comida, cocinar, preparar la cena, preparar el almuerzo, preparar la cena, preparar el almuerzo, preparar la comida"
+    },
+    {
+      name: "cricket",
+      description: "Competencia de cricket, juego de cricket, partido de cricket"
+    },
+    {
+      name: "cycling",
+      description: "Bicicleta, bicicletas, bicicleta, bicicletas, ciclismo, ciclismo, bicicleta de montaña, bicicleta de montaña"
+    },
+    {
+      name: "cyclingbmx",
+      description: "BMX"
+    },
+    {
+      name: "dancing",
+      description: "Ballet, danza, bailes, baile"
+    },
+    {
+      name: "datenight",
+      description: "Cena a la luz de las velas, cena a la luz de las velas, cita nocturna, cita nocturna, cena romántica"
+    },
+    {
+      name: "dentist",
+      description: "Dental, dentista, odontología, limpieza de dientes"
+    },
+    {
+      name: "dinner",
+      description: "Cena, cenas, comida familiar, restaurante, restaurantes"
+    },
+    {
+      name: "drinks",
+      description: "Despedida de soltera, cóctel, cócteles, bebidas, despedida de soltera , despedida de soltera, noche de damas, despedida de soltero, despedida de soltero, despedida de soltero, bar de vinos, noche de vino"
+    },
+    {
+      name: "equestrian",
+      description: "Doma clásica, Ecuestre, Concurso completo, Equitación, Equitación"
+    },
+    {
+      name: "fencing",
+      description: "Esgrima"
+    },
+    {
+      name: "fieldhockey",
+      description: "Hockey sobre hierba"
+    },
+    {
+      name: "gamenight",
+      description: "Juego de mesa, juegos de mesa, juego de mesa, juegos de mesa"
+    },
+    {
+      name: "genericnewyear",
+      description: "Año nuevo, año nuevo, año nuevo"
+    },
+    {
+      name: "golf",
+      description: "Golf"
+    },
+    {
+      name: "graduation",
+      description: "Graduación"
+    },
+    {
+      name: "gym",
+      description: "Crossfit, gimnasio, clase de fitness, evaluación de fitness, programa de fitness, prueba de fitness, entrenamiento físico, gimnasio, levantamiento de pesas, levantamiento de pesas, entrenamiento, entrenamientos"
+    },
+    {
+      name: "haircut",
+      description: "Cabello, corte de pelo, peluquería"
+    },
+    {
+      name: "halloween",
+      description: "Noche de Todos los Santos, Noche de Todos los Santos, Allhalloween, hallowe'en, halloween, helloween"
+    },
+    {
+      name: "handball",
+      description: "Balonmano"
+    },
+    {
+      name: "handcraft",
+      description: "Crochet, bordado, fieltrado, artesanía, tejido, sombrerería, patchwork, acolchado, costura"
+    },
+    {
+      name: "hiking",
+      description: "Caminatas, caminatas, senderismo"
+    },
+    {
+      name: "islamicnewyear",
+      description: "Año Nuevo Hijri, Año Nuevo Islámico, Año Nuevo Parsi"
+    },
+    {
+      name: "karate",
+      description: "Aikido, jiu jitsu, jiu-jitsu, judo, Jujutsu, karate, artes marciales, taekwondo"
+    },
+    {
+      name: "kayaking",
+      description: "Canoa, piragüismo, kayak"
+    },
+    {
+      name: "learninstrument",
+      description: "Violonchelo, coro, práctica de coro, clarinete, música clásica , contrabajo, corneta, flauta, lección de guitarra, clase de música, conjunto musical, oboe, orquesta, piano, saxofón, canto, cuarteto de cuerda, trombón, trompeta, tuba"
+    },
+    {
+      name: "learnlanguage",
+      description: "<idioma> Clase, <idioma> Curso, Práctica <idioma>, donde el idioma es uno de: árabe, búlgaro, catalán, chino, croata, checo, danés, holandés, inglés, farsi, filipino, finlandés, francés, alemán, griego, hebreo, hindi, húngaro, indonesio, italiano, japonés, coreano, Letón, Lituano, Noruego, Polaco, Portugués, Ruso, Eslovaco, Esloveno, Español, Sueco, Tailandés, Turco, Ucraniano, Vietnamita"
+    },
+    {
+      name: "lunch",
+      description: "Almuerzo, almuerzo, almuerzos"
+    },
+    {
+      name: "manicure",
+      description: "Manicura, manicura, pedicura, pedicura"
+    },
+    {
+      name: "mardigras",
+      description: "Martes Gordo, Mardi Gras, Mardigras, Martes de Carnaval"
+    },
+    {
+      name: "massage",
+      description: "Masaje de espalda, masaje de espalda, masajes"
+    },
+    {
+      name: "nowruz",
+      description: "Nowruz, Año Nuevo persa"
+    },
+    {
+      name: "oilchange",
+      description: "Servicio de coche, cambio de aceite"
+    },
+    {
+      name: "pingpong",
+      description: "Ping pong, ping-pong, pingpong, tenis de mesa"
+    },
+    {
+      name: "planmyday",
+      description: "Planificar día, planificar trimestre, planificar vacaciones, planificar semana, planificación de vacaciones , planificación de la semana"
+    },
+    {
+      name: "pride",
+      description: "Día de christopher street, marcha del dique, orgullo del euro, europride, gay y lesbiana, gay y lesbiana, lesbiana gay, desfile gay, orgullo gay, gaygler, gayglers, marcha lésbica, desfile lésbico, orgullo lésbico, orgullo mundial, orgullo mundial"
+    },
+    {
+      name: "quinceanera",
+      description: "Quinceañera"
+    },
+    {
+      name: "reachout",
+      description: "Contactar, enviar invitaciones, escribir cartas"
+    },
+    {
+      name: "read",
+      description: "Libro electrónico, periódico, lectura"
+    },
+    {
+      name: "repair",
+      description: "Bricolaje, electricista, reparación de refrigeradores, personal de mantenimiento, plomero"
+    },
+    {
+      name: "rhythmicgymnastics",
+      description: "Gimnasia rítmica"
+    },
+    {
+      name: "rowing",
+      description: "Head of the Charles, Head of the River Race, May Bumps"
+    },
+    {
+      name: "rugbysevens",
+      description: "Rugby"
+    },
+    {
+      name: "running",
+      description: "Trotar, trotar, trotar, correr, esprintar, atletismo, Atletismo"
+    },
+    {
+      name: "sailing",
+      description: "Crucero en barco, vela, velero, vela"
+    },
+    {
+      name: "saintpatricksday",
+      description: "San Patricio, San Patricio"
+    },
+    {
+      name: "santa",
+      description: "Papá Noel, Papá Noel"
+    },
+    {
+      name: "shooting",
+      description: "Tiro competitivo, competición de tiro, tiro deportivo, tiro deportes"
+    },
+    {
+      name: "skiing2",
+      description: "Esquí, esquí, esquí, snowboard, raquetas de nieve, snowboard, raquetas de nieve"
+    },
+    {
+      name: "sleep",
+      description: "Siesta, siesta, relajación, descanso, sueño, dormir"
+    },
+    {
+      name: "soccer",
+      description: "Fútbol"
+    },
+    {
+      name: "swimming",
+      description: "Buceo, natación, natación, natación, natación sincronizada"
+    },
+    {
+      name: "tennis",
+      description: "Tenis"
+    },
+    {
+      name: "thanksgiving",
+      description: "Acción de gracias"
+    },
+    {
+      name: "theatreopera",
+      description: "Ópera, teatro , teatro"
+    },
+    {
+      name: "triathlon",
+      description: "Triatlón"
+    },
+    {
+      name: "valentinesday",
+      description: "Día de San Valentín, Día de San Valentín, Día de San Valentín"
+    },
+    {
+      name: "videogaming",
+      description: "AGDQ, Games Done Quick, SGDQ, videojuego, videojuegos, videojuegos, Videojuegos, Videojuegos"
+    },
+    {
+      name: "violin2",
+      description: "Violín, violines"
+    },
+    {
+      name: "volleyball",
+      description: "Voleibol"
+    },
+    {
+      name: "walk",
+      description: "Salir a caminar, pasear"
+    },
+    {
+      name: "walkingdog",
+      description: "Cuidar perros , paseador de perros, paseador de perros, cuidado de perros, sacar al perro, sacar al perro, pasear al perro, pasear al perro"
+    },
+    {
+      name: "waterpolo",
+      description: "Waterpolo, waterpolo"
+    },
+    {
+      name: "wedding",
+      description: "Boda, víspera de boda, fiesta de víspera de boda, bodas"
+    },
+    {
+      name: "wrestling",
+      description: "Lucha libre"
+    },
+    {
+      name: "xmas",
+      description: "Boxing Day, Navidad, Navidad, Navidad"
+    },
+    {
+      name: "xmasmeal",
+      description: "Brunch <día festivo>, cena <día festivo>, almuerzo <día festivo>, almuerzo <día festivo>, donde <día festivo> es en Navidad, Nochebuena, Navidad, Navidad, x -mas, víspera de"
+    },
+    {
+      name: "xmasparty",
+      description: "Fiesta de víspera de Navidad, fiesta de Navidad, fiesta de víspera de Navidad, fiesta de Navidad, fiesta de víspera de Navidad, fiesta de Navidad"
+    },
+    {
+      name: "yoga",
+      description: "Yoga"
+    }
+]
+
